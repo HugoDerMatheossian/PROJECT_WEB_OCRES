fae8448da475201aaee64401c947cb7f74f4ce5b
HugoDerMatheossian <78907879+HugoDerMatheossian@users.noreply.github.com>
2022-11-24 20:47:58 +0100

    modif variées
    modif affichage infos des personnages
    modif du titre de la page affiché
    ajout d'une image pour tester

--- frontend/src/components/CharacterInfo.js
+++ frontend/src/components/CharacterInfo.js
@@ -7,11 +7,15 @@ const CharacterInfo = (props) => {
         <>
             <div className="character-content">
                 <h3>{name}</h3><br />
-                <h3>{name_kanji}</h3><br />
+                <div className="character-kanji">
+                    <h3>{name_kanji}</h3><br />
+                </div>
                 <img src={image_url} alt="" /><br /><br />
                 <div className="info-character">
                     {/* <h3>Anime:  </h3><br /> */}
-                    <h3><u>Nicknames:</u> {nicknames}</h3><br />
+                    <div className="character-nicknames">
+                        <h3><u>Nicknames:</u> {nicknames}</h3><br />
+                    </div>
                     <div className="character-about">
                         <h4><u>ABOUT:</u> <i>{about}</i></h4>
                     </div>
