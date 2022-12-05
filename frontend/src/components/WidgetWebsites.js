921c0c8bcabf66de89f69375e0d2e2dcc4e717ee
HugoDerMatheossian <78907879+HugoDerMatheossian@users.noreply.github.com>
2022-12-05 20:24:52 +0100

    5eme widget

--- frontend/src/components/WidgetWebsites.js
+++ frontend/src/components/WidgetWebsites.js
@@ -1,10 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import "./WidgetWebsites.css";
+import axios from "axios"
 
 export const WidgetWebsites = () => {
+
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        getWebsites();
+    }, [])
+
+    const getWebsites = async () => {
+        const response = await axios.get("http://localhost:4000/api/websites/getAll");
+        if (response.status === 200) {
+            setData(response.data);
+        }
+    };
+
+    console.log("data=>", data);
+
     return (
         <div className="widgetWebsite">
             <div className="header">
                 <u><h1>Best anime streaming Websites</h1></u>
+            </div>
+            <div className="websitesinfos">
+                <div>
+                    <table className="styled-table">
+                        <thead>
+                            <tr>
+                                <th style={{ textAlign: "center" }}>No.</th>
+                                <th style={{ textAlign: "center" }}>Website Name</th>
+                                <th style={{ textAlign: "center" }}>Note</th>
+                                <th style={{ textAlign: "center" }}>Nb Visits</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {data && data.map((item, index) => {
+                                return (
+                                    <tr key={index}>
+                                        <th scope='row'>{index + 1}</th>
+                                        <td>{item.websiteName}</td>
+                                        <td>{item.score}</td>
+                                        <td>{item.nbVisites}</td>
+                                    </tr>
+                                )
+                            })}
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     )
