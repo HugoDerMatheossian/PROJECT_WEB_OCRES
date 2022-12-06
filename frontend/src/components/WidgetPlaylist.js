68705d00e93e6c4a2ad6e70209138406a6350186
HugoDerMatheossian <78907879+HugoDerMatheossian@users.noreply.github.com>
2022-12-06 06:16:49 +0100

    6eme widget

--- frontend/src/components/WidgetPlaylist.js
+++ frontend/src/components/WidgetPlaylist.js
@@ -2,15 +2,62 @@ import React, { useEffect, useState } from 'react';
 import axios from "axios"
 import './css/video.css';
 
+const initialState = {
+    NameAnime: "",
+    URL: "",
+    Titre_video: "",
+};
+
+
 export const WidgetPlaylist = () => {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        getVideos();
+    }, []);
+
+    const getVideos = async () => {
+        const response = await axios.get(
+            "http://localhost:4000/api/videos/getAll"
+        );
+        if (response.status === 200) {
+            setData(response.data);
+        }
+    };
+
     return (
         <div className="playlistBody">
             <div className="header">
-                <u><h1>Our Anime Songs Selection</h1></u>
+                <u><h1>Our Anime Songs Playlist</h1></u>
             </div>
             <div className="VideoBox">
-                <div className="video"></div>
-                <button >RANDOM SONG</button>
+                <div className="video">
+                    <div className="InfosGeneral">
+                        <div className="ListVideo_Container">
+                            <table className="styled-table">
+                                <thead>
+                                    <tr>
+                                        <th>ANIME NAME</th>
+                                        <th>URL</th>
+                                        <th>VIDEO NAME</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {data &&
+                                        data.map((item, index) => {
+                                            return (
+                                                <tr key={index}>
+                                                    <td>{item.NameAnime}</td>
+                                                    <td><a href={item.URL} target={item.URL}>{item.URL}</a></td>
+                                                    <td>{item.Titre_video}</td>
+                                                </tr>
+                                            );
+                                        })}
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
